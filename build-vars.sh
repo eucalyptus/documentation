6848f7d08ebc7e8ecfc5ca6079bf02d30872328e
Scott Butler <scottb@eucalyptus.com>
2012-06-22 18:04:43 -0700

    Fixed DOC-347 - rewrote instructions for Centos6 / RHEL6 install. Also adding build-vars script for jenkins / local builds.

---
+++ build-vars.sh
@@ -0,0 +1,41 @@
+echo "Setting environment variables…"
+
+export DITA_HOME="`pwd`/DITA-OT"
+export DOC_HOME=`pwd`
+export ANT_HOME="$DITA_HOME/tools/ant"
+echo DITA_HOME IS $DITA_HOME
+echo DOC_HOME is $DOC_HOME
+echo ANT_HOME is $ANT_HOME
+
+# Get the absolute path of DITAOT's home directory
+cd "$DITA_HOME"
+DITA_DIR="`pwd`"
+echo DITA_DIR is $DITA_DIR
+cd ..
+
+# Make sure ant binary is executable
+if [ -f "$DITA_DIR"/tools/ant/bin/ant ] && [ ! -x "$DITA_DIR"/tools/ant/bin/ant ]; then
+echo "*** chmoding ant binary so it's executable ***"
+chmod +x "$DITA_DIR"/tools/ant/bin/ant
+fi
+
+echo "*** Setting ant environment variables ***"
+export ANT_OPTS="-Xmx512m $ANT_OPTS"
+export ANT_OPTS="$ANT_OPTS -Djavax.xml.transform.TransformerFactory=net.sf.saxon.TransformerFactoryImpl"
+#export ANT_HOME="$DITA_DIR"/tools/ant
+
+echo "*** Adding project-specific version of ant to path ***"
+export PATH="$DITA_DIR"/tools/ant/bin:"$PATH"
+
+echo "*** Adding new CLASSPATH items ***"
+NEW_CLASSPATH="$DITA_DIR/lib/saxon/saxon9.jar:$DITA_DIR/lib/saxon/saxon9-dom.jar:$DITA_DIR/lib:$DITA_DIR/lib/dost.jar:$DITA_DIR/lib/commons-codec-1.4.jar:$DITA_DIR/lib/resolver.jar:$DITA_DIR/lib/icu4j.jar"
+
+#check to see if classpath already exists - if so, append our new values
+if test -n "$CLASSPATH"
+then
+export CLASSPATH="$NEW_CLASSPATH":"$CLASSPATH"
+else
+export CLASSPATH="$NEW_CLASSPATH"
+fi
+ 
+
