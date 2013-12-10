7ce03c5765165b909cff8e873e0e7b47a1e6a7e6
scottb <scottb@eucalyptus.com>
2013-12-09 16:45:35 -0800

    Added troubleshooting guide to builds.

--- content/en_us/build-www.sh
+++ content/en_us/build-www.sh
@@ -4,7 +4,7 @@ echo "Building HTML docs:"
 [ -z "$PRODUCT_DIR" ] && export PRODUCT_DIR=../../../products
 export DOC_VER=3.3
 
-for DOC_NAME in admin-guide console-guide euca2ools-guide faststart-guide hybrid-guide install-guide release-notes release-notes-euca2ools user-guide; do
+for DOC_NAME in admin-guide console-guide euca2ools-guide faststart-guide hybrid-guide install-guide release-notes release-notes-euca2ools troubleshooting-guide user-guide; do
     echo "Building $DOC_NAME..."
     mkdir $PRODUCT_DIR/en_us/www/$DOC_NAME
     $DITAC_HOME/bin/ditac  -f webhelp  -images img  -p use-note-icon yes  -p xsl-resources-directory css  -toc  -p chain-pages both  -p wh-jquery-ui ../../jquery-ui/js/jquery-ui-1.8.23.custom.min.js  -p wh-jquery-css ../../jquery-ui/css/custom-theme/jquery-ui-1.8.23.custom.css  -p wh-user-header ./whc_template/_wh/euca/header_$DOC_NAME.html  -p wh-user-resources ./whc_template/_wh/euca  -p wh-user-css ./whc_template/_wh/euca/eucadoc.css  -p wh-user-footer ./whc_template/_wh/euca/footer.html  -p wh-collapse-toc yes  -index  $PRODUCT_DIR/en_us/www/$DOC_NAME/_.html $DOC_HOME/en_us/$DOC_NAME.ditamap
