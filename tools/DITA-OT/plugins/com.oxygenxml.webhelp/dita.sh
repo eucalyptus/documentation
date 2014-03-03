0974b61cce9f3ac4c0704884b23a5b3821a90e6b
Scot Marvin <scot.marvin@eucalyptus.com>
2014-03-03 08:46:01 -0800

    Updating build script.

--- tools/DITA-OT/plugins/com.oxygenxml.webhelp/dita.sh
+++ tools/DITA-OT/plugins/com.oxygenxml.webhelp/dita.sh
@@ -7,13 +7,13 @@
 
 
 # The path of the Java Virtual Machine install directory
-JVM_INSTALL_DIR=/usr/bin/jdk1.6.0_38
+JVM_INSTALL_DIR=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
 
 # The path of the DITA Open Toolkit install directory
-DITA_OT_INSTALL_DIR=/home/test/oxygen-webhelp/DITA-OT1.7.5
+DITA_OT_INSTALL_DIR=$DITA_HOME
 
 # The path of the Saxon 9.1.0.8 install directory  
-SAXON_9_DIR=/home/test/oxygen-webhelp/saxonb9-1-0-8j
+SAXON_9_DIR=/Users/scot-macbook15/Documents/euca/tools/saxonb9-1-0-8j
 
 # One of the following three values: 
 #      webhelp
@@ -22,16 +22,17 @@ SAXON_9_DIR=/home/test/oxygen-webhelp/saxonb9-1-0-8j
 TRANSTYPE=webhelp
 
 # The path of the directory of the input DITA map file
-DITA_MAP_BASE_DIR=/home/test/oxygen-webhelp/OxygenXMLEditor/samples/dita/flowers
+# DITA_MAP_BASE_DIR=/home/test/oxygen-webhelp/OxygenXMLEditor/samples/dita/flowers
+DITA_MAP_BASE_DIR=$DOC_HOME/en_us
 
 # The name of the input DITA map file
-DITAMAP_FILE=flowers.ditamap
+DITAMAP_FILE=documentation.ditamap
 
 # The name of the DITAVAL input filter file 
 DITAVAL_FILE=my_ditaval.ditaval
 
 # The path of the directory of the DITAVAL input filter file
-DITAVAL_DIR=/home/test/oxygen-webhelp/OxygenXMLEditor/samples/dita
+DITAVAL_DIR=/Applications/oxygen/samples/dita
 
 
 "$JVM_INSTALL_DIR/bin/java"\
