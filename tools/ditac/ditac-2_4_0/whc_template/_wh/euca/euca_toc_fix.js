f83cdb089a39f8752f3335b7c93459e3f48a649c
Scot Marvin <scot.marvin@eucalyptus.com>
2014-06-03 08:57:20 -0700

    Removing ditac from repo: we no longer use these transforms

--- tools/ditac/ditac-2_4_0/whc_template/_wh/euca/euca_toc_fix.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var childTOC = $( ".wh-toc-selected" );
-    var x = $(childTOC).parent();
-    if (x.children().length > 1) {
-        x.removeClass("toc-collapsed").addClass("toc-expanded");
-        x.children().css('display','block');
-    }
-});
